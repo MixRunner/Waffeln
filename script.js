39fed2caa83c737e77206576536f605eb66d2e85
MixRunner <sintaxiserror5@gmail.com>
2023-03-31 20:28:05 +0300

    Committed

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 function generateRandomNumber() {
     const minValue = parseInt(document.getElementById("minValue").value);
     const maxValue = parseInt(document.getElementById("maxValue").value);
+    const numberType = document.querySelector('input[name="numberType"]:checked').value;
+    const includeBoundaries = document.getElementById("includeBoundaries").checked;
 
     if (isNaN(minValue) || isNaN(maxValue)) {
         alert("Будь ласка, введіть коректні числа");
         return;
     }
 
-    const result = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    let min = minValue;
+    let max = maxValue;
+
+    if (!includeBoundaries) {
+        min = minValue + 1;
+        max = maxValue - 1;
+    }
+
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (numberType === 'even') {
+        result = result % 2 === 0 ? result : result - 1;
+        if (result < min) {
+            result += 2;
+        }
+    } else if (numberType === 'odd') {
+        result = result % 2 !== 0 ? result : result - 1;
+        if (result < min) {
+            result += 2;
+        }
+    }
+
     document.getElementById("result").innerText = result;
 }
